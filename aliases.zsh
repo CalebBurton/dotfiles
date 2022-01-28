12cce10e20cd634fb641cae20f4a6903297e68ce
Caleb Burton <ccburton4@gmail.com>
2022-01-28 10:55:00 -0500

    feat(aliases): switch to double quotes

--- aliases.zsh
+++ aliases.zsh
@@ -1,32 +1,33 @@
-alias watch-agora='cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/agora && echo "yarn watch" && yarn watch'
-alias watch-arachne='cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/arachne && source .env && echo "yarn watch" && yarn watch'
-alias build-arachne='cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/arachne && source .env && echo "yarn build" && yarn build'
-alias start-ithaca='cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/ithaca && echo "yarn start" && yarn start'
+alias watch-agora="cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/agora && echo 'yarn watch' && yarn watch"
+alias watch-arachne="cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/arachne && source .env && echo 'yarn watch' && yarn watch"
+alias build-arachne="cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/arachne && source .env && echo 'yarn build' && yarn build"
+alias start-ithaca="cd ~/Documents/GitLab/umbrella && source .env && cd ./apps/ithaca && echo 'yarn start' && yarn start"
 
-alias lint-umbrella='cd ~/Documents/GitLab/umbrella && source .env && echo "yarn lint" && yarn lint'
-alias test-umbrella='cd ~/Documents/GitLab/umbrella && source .env && echo "yarn test" && yarn test'
+alias lint-umbrella="cd ~/Documents/GitLab/umbrella && source .env && echo 'yarn lint' && yarn lint"
+alias test-umbrella="cd ~/Documents/GitLab/umbrella && source .env && echo 'yarn test' && yarn test"
 
-alias start-specialist='cd ~/Documents/GitLab/specialist && echo "nix-shell (\"make server\" starts things)" && nix-shell'
-alias test-specialist='cd ~/Documents/GitLab/specialist && echo "bundle exec guard (\"rspec\" command runs all tests)" && bundle exec guard'
+alias start-specialist="cd ~/Documents/GitLab/specialist && echo 'nix-shell (\"make server\" starts things)' && nix-shell"
+alias test-specialist="cd ~/Documents/GitLab/specialist && echo 'bundle exec guard (\"rspec\" command runs all tests)' && bundle exec guard"
 
-alias start-navigator='cd ~/Documents/GitLab/navigator && echo "bundle exec rails s -p 3001 (I hope specialist is running)" && bundle exec rails s -p 3001'
-alias start-papuros='cd ~/Documents/GitLab/papuros && echo "yarn start -p 3002" && yarn start -p 3002'
+alias start-navigator="cd ~/Documents/GitLab/navigator && echo 'bundle exec rails s -p 3001 (I hope specialist is running)' && bundle exec rails s -p 3001"
+alias start-papuros="cd ~/Documents/GitLab/papuros && echo 'yarn start -p 3002' && yarn start -p 3002"
 
-alias dcu='docker-compose up'
-alias dcb='docker-compose build'
+alias dcu="docker-compose up"
+alias dcb="docker-compose build"
 
-alias sad='say -v karen "all done"'
+alias sad="say -v karen 'all done'"
 
-alias website-go='cd ~/Documents/GitHub/website-source && npm run start:dev'
-alias website-publish='cd ~/Documents/GitHub/website-source && npm run publish'
+alias website-go="cd ~/Documents/GitHub/website-source && npm run start:dev"
+alias website-publish="cd ~/Documents/GitHub/website-source && npm run publish"
 
 alias gpl="git pull"
 alias gps="git push"
-alias gs='git stash'
-alias gsp='git stash pop'
-alias grim='git rebase -i --autosquash origin/master'
-alias gamend='git add -A && git commit -a --amend -C HEAD'
-alias gcom='git commit -m'
+alias gpsh="git push"
+alias gs="git stash"
+alias gsp="git stash pop"
+alias grim="git rebase -i --autosquash origin/master"
+alias gamend="git add -A && git commit -a --amend -C HEAD"
+alias gcom="git commit -m"
 # Delete a file and all history of it
 alias gdestroy="git ls-files --ignored --exclude-standard | xargs -0 git rm -r"
 
@@ -35,9 +36,10 @@ alias gst="git status -s -b"
 alias glog="glol"
 alias gap="git add --patch"
 alias gdc="git diff --cached"
-alias gp='echo "Error: \"gp\" is ambiguous. Use \"gpl\" to pull or \"gps\" to push." && return 1'
+alias gp="echo 'Error: \"gp\" is ambiguous. Use \"gpl\" to pull or \"gps\" to push.' && return 1"
+alias gpu="echo 'Error: \"gpu\" is ambiguous. Use \"gpl\" to pull or \"gps\" to push.' && return 1"
 
-alias be='bundle exec'
+alias be="bundle exec"
 
 function eod() {
     cd ~/Documents/GitHub/personal
@@ -56,37 +58,37 @@ function bup() {
     #
     ## OLD VERSION
     #
-    echo 'Updating homebrew...\n'
+    echo "Updating homebrew...\n"
     brew update
-    echo ''
+    echo ""
     brew outdated --cask --greedy
     while true; do
         echo -n "\nUpgrade outdated casks? [Y/n]: "
         read yn
         case $yn in
             [Nn]* ) break;;
-            * ) echo '' && brew upgrade --cask --greedy;;
+            * ) echo "" && brew upgrade --cask --greedy;;
         esac
-        yn=''
+        yn=""
         break;
     done
-    echo ''
+    echo ""
     return 0;
 }
 
-alias bs='brew search'
-alias bsc='brew search --casks'
+alias bs="brew search"
+alias bsc="brew search --casks"
 
-# `bc` is already a command, don't overwrite it
-alias bic='brew install --cask'
-alias buc='brew upgrade --cask'
-alias bucg='brew upgrade --cask --greedy'
+# `bc` is already a command, don"t overwrite it
+alias bic="brew install --cask"
+alias buc="brew upgrade --cask"
+alias bucg="brew upgrade --cask --greedy"
 
-alias please='sudo'
-alias reload='source $HOME/.zshrc'
+alias please="sudo"
+alias reload="source $HOME/.zshrc"
 
-alias zshrc='code $HOME/Documents/GitHub/dotfiles/.zshrc'
-alias aliases='code $HOME/Documents/GitHub/dotfiles/aliases.zsh'
-alias tmuxconf='code $HOME/Documents/GitHub/dotfiles/.tmux.conf'
+alias zshrc="code $HOME/Documents/GitHub/dotfiles/.zshrc"
+alias aliases="code $HOME/Documents/GitHub/dotfiles/aliases.zsh"
+alias tmuxconf="code $HOME/Documents/GitHub/dotfiles/.tmux.conf"
 
-alias python='python3'
+alias python="python3"
