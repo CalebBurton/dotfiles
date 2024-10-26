59f3870f3aa777c18dd71392acbfe36e1d57f141
Caleb Burton <ccburton4@gmail.com>
2024-10-26 13:04:36 -0400

    feat(nix): update pkgs and add nixos-boot

--- aliases.zsh
+++ aliases.zsh
@@ -58,6 +58,8 @@ alias nixos-build="echo 'sudo nixos-rebuild build --flake $FLAKE_LOCATION#' \
     && sudo nixos-rebuild build --flake $FLAKE_LOCATION#"
 alias nixos-switch="echo 'sudo nixos-rebuild switch --flake $FLAKE_LOCATION#' \
     && sudo nixos-rebuild switch --flake $FLAKE_LOCATION#"
+alias nixos-boot="echo 'sudo nixos-rebuild boot --flake $FLAKE_LOCATION#' \
+    && sudo nixos-rebuild boot --flake $FLAKE_LOCATION#"
 alias nixos-rollback="echo 'sudo nixos-rebuild switch --rollback --flake $FLAKE_LOCATION#' \
     && sudo nixos-rebuild switch --rollback --flake $FLAKE_LOCATION#"
 alias nixos-trim-dry="echo 'sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 14d --dry-run' \
